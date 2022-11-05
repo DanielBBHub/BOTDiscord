db8e664ae538f3f8605faaee579e5e615dacb34d
Daniel Benavides Belmonte <ruxzfly@gmail.com>
2022-11-05 19:13:03 +0100

    Responde a "hola"

--- cliente.py
+++ cliente.py
@@ -7,3 +7,10 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+
+        
+
+        if self.user.mentioned_in(message):
+            if "hola" in message.content:
+                print(message.content)
+                await message.channel.send("Pa ti mi cola")
