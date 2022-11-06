661705493171e1f64853092bfc7212628dbcbec7
Daniel Benavides Belmonte <ruxzfly@gmail.com>
2022-11-06 16:47:24 +0100

    Cambio de splits() por una REGEX

--- cliente.py
+++ cliente.py
@@ -1,27 +1,33 @@
 
 import discord
 import calendario as c
+
+# Libreria de la que extraemos funciones de expresiones regulares
+import re
+
 class MyClient(discord.Client):
+    
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
         c.Calendario()
+        self.REGEX = "^\$([a-z]*) !([0-9]*-[0-9]*-[0-9]*) ([A-Z]*[a-z]+) ([A-Z]*[a-z]+)"
+        
+       
         
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
         
+        match = re.match(self.REGEX, message.content)
+        if match:
+            print("Comando: {0} \nArgumentos: {1}".format(match.groups()[0], match.groups()[1:]))
 
         if self.user.mentioned_in(message):
             if "hola" in message.content:
-                print(message.content)
                 await message.channel.send("Pa ti mi cola")
 
-            if "$" in message.content:
-                mensaje = message.content.split("$")
-                print(f"Mensaje recibido: {mensaje} ")
-                if "examen" in mensaje[1]:
-                    print(f"Comando recogido: {mensaje[1]} ")
-                    args = mensaje[1].split("!")
-                    args = args[1].split(" ")
-                    print(f"Examen recogido: {args}")
+
+            
+
+            
                     
